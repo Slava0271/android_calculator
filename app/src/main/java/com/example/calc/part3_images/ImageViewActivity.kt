e070dce4f85b91c32830184ffa5bd53ed3ed524b
Slava <kergakovslava@gmail.com>
2021-03-03 19:52:11 +0200

    add some transformation

--- app/src/main/java/com/example/calc/part3_images/ImageViewActivity.kt
+++ app/src/main/java/com/example/calc/part3_images/ImageViewActivity.kt
@@ -1,11 +1,15 @@
 package com.example.calc.part3_images
 
+import android.annotation.SuppressLint
+import android.media.projection.MediaProjectionManager
+import android.media.session.MediaSessionManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.calc.R
@@ -15,6 +19,7 @@ import jp.wasabeef.glide.transformations.BlurTransformation
 import jp.wasabeef.glide.transformations.GrayscaleTransformation
 import jp.wasabeef.glide.transformations.MaskTransformation
 import kotlinx.android.synthetic.main.activity_image_view.*
+import java.lang.NullPointerException
 
 class ImageViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +27,7 @@ class ImageViewActivity : AppCompatActivity() {
         setContentView(R.layout.activity_image_view)
         supportActionBar?.hide()
         addSpinner()
+        ifItemSelected()
         drawFirstImage()
         buttonListeners()
 
@@ -53,45 +59,85 @@ class ImageViewActivity : AppCompatActivity() {
 
     private fun setImages(count: Int) {
         when (count) {
-            0 -> imageViewChangeColor.setImageResource(R.drawable.cat)
-            1 -> imageViewChangeColor.setImageResource(R.drawable.shark)
-            2 -> loadImageFromURL(getString(R.string.mouse_img), imageViewChangeColor)
-            3 -> loadImageFromURL(getString(R.string.goat_img), imageViewChangeColor)
-            4 -> imageViewChangeColor.setImageResource(R.drawable.horse)
+            0 -> {
+                imageViewChangeColor.setImageResource(R.drawable.cat)
+                imageViewChangeColor.tag = "cat"
+            }
+            1 -> {
+                imageViewChangeColor.setImageResource(R.drawable.shark)
+                imageViewChangeColor.tag = "shark"
+            }
+            2 -> {
+                loadImageFromURL(getString(R.string.mouse_img), imageViewChangeColor)
+                imageViewChangeColor.tag = "mouse"
+            }
+            3 -> {
+                loadImageFromURL(getString(R.string.goat_img), imageViewChangeColor)
+                imageViewChangeColor.tag = "goat"
+            }
+            4 -> {
+                imageViewChangeColor.setImageResource(R.drawable.horse)
+                imageViewChangeColor.tag = "horse"
+            }
         }
     }
 
-    private fun loadImageFromURL(url: String, imageView: ImageView) {
-        Glide.with(this).load(url).into(imageView)
+
+    private fun loadImageFromURL(url: String, imageView: ImageView): Any {
+        return Glide.with(this).load(url).into(imageView)
     }
 
-    private fun blurTransformation() {
-        Glide.with(this).load(R.drawable.cat)
-            .apply(RequestOptions.bitmapTransform(BlurTransformation(25, 3)))
-            .into(imageViewChangeColor)
-    }
-    private fun ifItemSelected(){
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+    private fun ifItemSelected() {
+        var selectedItem:Int =0
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
+                if(selectedItem==1)
+                    blurTransformation()
+                if(selectedItem==2)
+                    grayscaleTransformation()
 
             }
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-
+                selectedItem=position
+                //  2 -> grayscaleTransformation()
+                if (position == 1)
+                    blurTransformation()
+                else if (position == 2)
+                    grayscaleTransformation()
             }
 
         }
     }
 
+    private fun getTag(): Any {
+        var any: Any = ""
+
+        when (imageViewChangeColor.tag) {
+            "cat" -> any = R.drawable.cat
+            "mouse" -> any = getString(R.string.mouse_img)
+            "goat" -> any = getString(R.string.goat_img)
+            "shark" -> any = R.drawable.shark
+            "horse" -> any = R.drawable.horse
+        }
+        return any
+    }
+
+    private fun blurTransformation() {
+        Glide.with(this).load(getTag())
+                .apply(RequestOptions.bitmapTransform(BlurTransformation(25, 3)))
+                .into(imageViewChangeColor)
+    }
+
     private fun grayscaleTransformation() {
-        Glide.with(this).load(R.drawable.cat)
-            .apply(RequestOptions.bitmapTransform(GrayscaleTransformation()))
-            .into(imageViewChangeColor)
+        Glide.with(this).load(getTag())
+                .apply(RequestOptions.bitmapTransform(GrayscaleTransformation()))
+                .into(imageViewChangeColor)
     }
 
     private fun addSpinner() {
         val arrayAdapter: ArrayAdapter<CharSequence> = ArrayAdapter.createFromResource(
-            this, R.array.transformation, android.R.layout.simple_spinner_item
+                this, R.array.transformation, android.R.layout.simple_spinner_item
         )
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = arrayAdapter
