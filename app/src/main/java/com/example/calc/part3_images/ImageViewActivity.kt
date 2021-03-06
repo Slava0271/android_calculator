c35d9d290521cd00dcd1aba7778f171b0ebe936f
Slava <kergakovslava@gmail.com>
2021-03-06 11:55:46 +0200

    work with cache and progress bar

--- app/src/main/java/com/example/calc/part3_images/ImageViewActivity.kt
+++ app/src/main/java/com/example/calc/part3_images/ImageViewActivity.kt
@@ -1,12 +1,16 @@
 package com.example.calc.part3_images
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 import com.example.calc.R
 import com.example.calc.part8_change_color.ChangeActivityColors
@@ -14,20 +18,25 @@ import com.example.calc.part8_change_color.SwitchStatus
 import jp.wasabeef.glide.transformations.*
 import kotlinx.android.synthetic.main.activity_image_view.*
 
+
 class ImageViewActivity : AppCompatActivity() {
+    //A variable that is equal to the position of the selected item of the spinner
     var getPosition = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_view)
+
+        //change theme
+        val changeActivityColors: ChangeActivityColors = ChangeActivityColors()
+        if (SwitchStatus.isSwitchButton())
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
         supportActionBar?.hide()
+        loadAllURLFiles()
         buttonListeners()
         addSpinner()
         ifItemSelected()
-
-        val changeActivityColors: ChangeActivityColors = ChangeActivityColors()
-        if (SwitchStatus.isSwitchButton())
-            changeActivityColors.changeToLightTheme(animals_images)
-        else changeActivityColors.changeToDarkTheme(animals_images)
 
     }
 
@@ -38,6 +47,7 @@ class ImageViewActivity : AppCompatActivity() {
      */
     private fun buttonListeners() {
         var count = 0;
+        setImagesAndTransformation(count)
         buttonNext.setOnClickListener {
             if (count < 5)
                 count++
@@ -50,6 +60,7 @@ class ImageViewActivity : AppCompatActivity() {
             setImagesAndTransformation(count)
         }
     }
+
 
     /**
      *A method in which, depending on the count parameter,
@@ -68,12 +79,12 @@ class ImageViewActivity : AppCompatActivity() {
                 transformation(getPosition)
             }
             2 -> {
-                loadImageFromURL(getString(R.string.mouse_img), imageViewChangeColor)
+                displayImageFromCache(getString(R.string.mouse_img), imageViewChangeColor)
                 imageViewChangeColor.tag = "mouse"
                 transformation(getPosition)
             }
             3 -> {
-                loadImageFromURL(getString(R.string.goat_img), imageViewChangeColor)
+                displayImageFromCache(getString(R.string.goat_img), imageViewChangeColor)
                 imageViewChangeColor.tag = "goat"
                 transformation(getPosition)
             }
@@ -86,10 +97,46 @@ class ImageViewActivity : AppCompatActivity() {
     }
 
 
-    private fun loadImageFromURL(url: String, imageView: ImageView): Any {
-        return Glide.with(this).load(url).into(imageView)
+    private fun loadImageFromURL(url: String, imageView: ImageView) {
+        Glide.with(this)
+                .load(url)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(e: GlideException?, model: Any?, target: com.bumptech.glide.request.target.Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+
+                    override fun onResourceReady(resource: Drawable?, model: Any?, target: com.bumptech.glide.request.target.Target<Drawable>?, dataSource: com.bumptech.glide.load.DataSource?, isFirstResource: Boolean): Boolean {
+                        progressBar.visibility = View.GONE
+                        return false
+                    }
+                })
+                .into(imageView)
+
+
+        // return Glide.with(this).load(url).diskCacheStrategy(DiskCacheStrategy.ALL).into(imageView)
     }
 
+    private fun displayImageFromCache(url: String, imageView: ImageView) {
+        Glide.with(this)
+                .load(url)
+                .onlyRetrieveFromCache(true)
+                .into(imageView);
+    }
+
+    /**
+     * The method that downloads pictures from the Internet when activating
+     */
+    private fun loadAllURLFiles() {
+        loadImageFromURL(getString(R.string.mouse_img), imageViewChangeColor)
+        loadImageFromURL(getString(R.string.goat_img), imageViewChangeColor)
+    }
+
+
+    /**
+     * The method that applies transformation
+     * when changing the item of the spinner
+     */
     private fun ifItemSelected() {
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -120,6 +167,7 @@ class ImageViewActivity : AppCompatActivity() {
         else if (position == 0)
             noTransformation()
     }
+
 
     /**
      * A method that returns an image depending on the tag
@@ -172,5 +220,5 @@ class ImageViewActivity : AppCompatActivity() {
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = arrayAdapter
     }
-    
+
 }
--- app/src/main/java/com/example/calc/part4_title/TitleActivity.kt
+++ app/src/main/java/com/example/calc/part4_title/TitleActivity.kt
@@ -4,8 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.calc.R
 
-class
-TitleActivity : AppCompatActivity() {
+class TitleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_title)
